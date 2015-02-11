507c5aed440f8fa43633e6a19f528989724dd16c
Yannis Richard <yannisrichard63@gmail.com>
2015-02-11 19:56:38 +0100

    Correct render error

--- app.js
+++ app.js
@@ -11,10 +11,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 
-
 var app = express();
-
-//app.use(express.static(path.join(__dirname, 'public')));
 
 nunjucks.configure('views', {
     autoescape: true,
@@ -31,7 +28,7 @@ mongoose.connect('mongodb://admin:admin@ds063879.mongolab.com:63879/urlshortnerb
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html');
 
-app.use(logger('dev'));
+//app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -39,7 +36,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 
-// catch 404 and forward to error handler
+/// catch 404 and forward to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
@@ -62,16 +59,15 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-/*app.use(function(err, req, res, next) {
+app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
         error: {}
     });
-});*/
-app.use(function (req, res, next) {
-    res.status(404).render('404.html');
 });
+
+
 
 
 module.exports = app;
